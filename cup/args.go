66ca55289aa06a04f5ecbf6904a06094694aa7a3
leaffly <sherlock@bakerstreet.club>
2019-05-24 12:18:42 +0800

    add parser

--- cup/args.go
+++ cup/args.go
@@ -1,8 +1,13 @@
+/*
+ * @Author : leaf.fly
+ * @Create : 2019-05-24 8:23
+ * @Desc : this is class named args.go for do args.go
+ * @Version : v1.0.0.20190524
+ * @Github : http://github.com/laiyefei
+ * @Blog : http://laiyefei.com
+ * @OtherWebSite : http://bakerstreet.club;http://sherlock.help;
+ */
 package cup
 
-import "reflect"
-
-type ArgParse interface {
-	Parse(env *ctx, typ reflect.Type) (reflect.Value, bool)
-}
-
+import (
+    )
